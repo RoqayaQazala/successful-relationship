25f7208db9bb7cc6ec48c642fdbcb51bf4e7ae70
RoqayaQazala <roukamohammad@gmail.com>
2016-12-21 16:57:57 +0200

    first draft

---
+++ main.js
@@ -0,0 +1,11 @@
+$("body").css({"background-color": "#FAEBD7", "font-size": "200%"})
+$('li').css('color','#50F2BC')
+$("ul").css("text-color","red")
+$("ul").css("float" ,"middle")
+$("input").css({"width":400, "height":50, "font-size":30})
+$("h3").css("font-family","Comic Sans MS")
+$("#pair").css("float","right")
+$("img").css({"width":200, "height":200})
+
+
+
